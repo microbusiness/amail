e0b3a735e871a7743d51cefbc460bb9a9b6e5f3a
Ivan_G <mbusiness.iv@gmail.com>
2019-01-10 15:38:07 +0300

    fix

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,9 +9,113 @@ class User extends BaseUser
 {
     protected $id;
 
+    /**
+     * @var string
+     */
+    protected $fullname;
+
+    /**
+     * @var string
+     */
+    protected $phone;
+
+    /**
+     * @var boolean
+     */
+    protected $locked;
+
+    /**
+     * @var boolean
+     */
+    protected $expired;
+    /**
+     * @var boolean
+     */
+    protected $credentialsExpired;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFullname()
+    {
+        return $this->fullname;
+    }
+
+    /**
+     * @param mixed $fullname
+     */
+    public function setFullname($fullname): void
+    {
+        $this->fullname = $fullname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param string $phone
+     */
+    public function setPhone(string $phone): void
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isLocked(): bool
+    {
+        return $this->locked;
+    }
+
+    /**
+     * @param bool $locked
+     */
+    public function setLocked(bool $locked): void
+    {
+        $this->locked = $locked;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isExpired(): bool
+    {
+        return $this->expired;
+    }
+
+    /**
+     * @param bool $expired
+     */
+    public function setExpired(bool $expired): void
+    {
+        $this->expired = $expired;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isCredentialsExpired(): bool
+    {
+        return $this->credentialsExpired;
+    }
+
+    /**
+     * @param bool $credentialsExpired
+     */
+    public function setCredentialsExpired(bool $credentialsExpired): void
+    {
+        $this->credentialsExpired = $credentialsExpired;
     }
 }
